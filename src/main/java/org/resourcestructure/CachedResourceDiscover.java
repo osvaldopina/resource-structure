804848d88c3c7f948c1749945d116cfc58854fd1
osvaldo.pina <osvaldo.pina@gmail.com>
2013-05-31 16:38:16 -0300

    add default properties to cache

--- src/main/java/org/resourcestructure/CachedResourceDiscover.java
+++ src/main/java/org/resourcestructure/CachedResourceDiscover.java
@@ -1,43 +1,76 @@
 package org.resourcestructure;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class CachedResourceDiscover implements ResourceDiscover{
-    
-    private Map<Method,Structure> identities = new HashMap<Method,Structure>();
-    private Map<Method,Operation> operations = new HashMap<Method,Operation>();
-    private ResourceDiscover next;
-    
-    public CachedResourceDiscover(ResourceDiscover next) {
-        this.next = next;
-    }
-    
-    @Override
-    public Structure getStructure(Method method) {
-        Structure identity = identities.get(method);
-        
-        if (identity == null)  {
-            identity = next.getStructure(method);
-            if (identity != null) {
-                identities.put(method, identity);
-            }
-        }
-        return identity;
-    }
+public class CachedResourceDiscover implements ResourceDiscover {
 
-    @Override
-    public Operation getOperation(Method method) {
-        Operation operation = operations.get(method);
-        
-        if (operation == null)  {
-            operation = next.getOperation(method);
-            if (operation != null) {
-                operations.put(method, operation);
-            }
-        }
-        return operation;
-    }
+	private Map<Method, Structure> identities = new HashMap<Method, Structure>();
+	private Map<Method, Map<String, Attribute>> attributesToMerge = new HashMap<Method, Map<String, Attribute>>();
+	private Map<Method, Operation> operations = new HashMap<Method, Operation>();
+	private ResourceDiscover next;
+
+	public CachedResourceDiscover(ResourceDiscover next) {
+		this.next = next;
+	}
+
+	public void addAttributeToMerge(Method method, Attribute attribute) {
+		Map<String, Attribute> methodAttributes = attributesToMerge.get(method);
+
+		if (methodAttributes == null) {
+			methodAttributes = new HashMap<String, Attribute>();
+			attributesToMerge.put(method, methodAttributes);
+		}
+
+		methodAttributes.put(attribute.getName(), attribute);
+	}
+
+	@Override
+	public Structure getStructure(Method method) {
+		Structure identity = identities.get(method);
+
+		if (identity == null) {
+			identity = next.getStructure(method);
+			if (identity != null) {
+				identity = identity.merge(getStructureToMerge((method)));
+				identities.put(method, identity);
+			}
+		}
+		return identity;
+	}
+
+	private Structure getStructureToMerge(Method method) {
+		Map<String, Attribute> attributeMapToMergeForMethod = attributesToMerge.get(method);
+		
+		if (attributeMapToMergeForMethod == null) {
+			return null;
+		}
+		else {
+			List<Attribute> attributeListToMergeForMethod = new ArrayList<Attribute>(attributeMapToMergeForMethod.values());
+			if (attributeListToMergeForMethod.size() == 0) {
+				return null;
+			}
+			else {
+				return Structure.create(attributeListToMergeForMethod);
+			}
+		}
+	}
+
+	@Override
+	public Operation getOperation(Method method) {
+		Operation operation = operations.get(method);
+
+		if (operation == null) {
+			operation = next.getOperation(method);
+			if (operation != null) {
+				operations.put(method, operation);
+			}
+		}
+		return operation;
+	}
 
 }
+ 
--- src/test/java/org/resourcestructure/CachedResourceDiscoverTest.java
+++ src/test/java/org/resourcestructure/CachedResourceDiscoverTest.java
@@ -2,10 +2,12 @@ package org.resourcestructure;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Method;
 
 import org.junit.Test;
+import org.resourcestructure.annotations.AttributeName;
 import org.resourcestructure.annotations.OperationType;
 
 
@@ -27,6 +29,22 @@ public class CachedResourceDiscoverTest {
     }
     
     @Test
+    public void getResourceIdNextReturningNullWithAttributesToMerge() throws Exception {
+        Method method = ResourceDiscoverMock.class.getMethod("m1");
+        ResourceDiscoverMock resourceDiscoverMock = new ResourceDiscoverMock();
+        
+        CachedResourceDiscover cachedResourceDiscover = new CachedResourceDiscover(resourceDiscoverMock);
+        cachedResourceDiscover.addAttributeToMerge(method, new Attribute(AttributeName.VERSION,"1.0.0"));
+        
+        assertNull(cachedResourceDiscover.getStructure(method));
+        assertEquals(1,resourceDiscoverMock.timesGetResoureId);
+        assertEquals(0,resourceDiscoverMock.timesGetOperation);
+
+        assertEquals(method,resourceDiscoverMock.lastMethodParameter);
+    }
+
+    
+    @Test
     public void getResourceIdNextReturningNonNull() throws Exception {
         Method method = ResourceDiscoverMock.class.getMethod("m1");
         ResourceDiscoverMock resourceDiscoverMock = new ResourceDiscoverMock();
@@ -34,19 +52,63 @@ public class CachedResourceDiscoverTest {
         
         // first call
         CachedResourceDiscover cachedResourceDiscover = new CachedResourceDiscover(resourceDiscoverMock);
+        Structure cachedStructure =  cachedResourceDiscover.getStructure(method);
         
-        assertEquals(resourceDiscoverMock.structure, cachedResourceDiscover.getStructure(method));
+        assertEquals(resourceDiscoverMock.structure.getName(), cachedStructure.getName());
+        assertTrue(resourceDiscoverMock.structure.getAttributes().isEmpty());
+        assertTrue(cachedStructure.getAttributes().isEmpty());
+
         assertEquals(1,resourceDiscoverMock.timesGetResoureId);
         assertEquals(0,resourceDiscoverMock.timesGetOperation);
         assertEquals(method,resourceDiscoverMock.lastMethodParameter);
         
         // second call
-        assertEquals(resourceDiscoverMock.structure, cachedResourceDiscover.getStructure(method));
+        assertEquals(resourceDiscoverMock.structure.getName(), cachedStructure.getName());
+        assertTrue(resourceDiscoverMock.structure.getAttributes().isEmpty());
+        assertTrue(cachedStructure.getAttributes().isEmpty());
+
         assertEquals(1,resourceDiscoverMock.timesGetResoureId);
         assertEquals(0,resourceDiscoverMock.timesGetOperation);
         assertEquals(method,resourceDiscoverMock.lastMethodParameter);
         
     }
+    
+    @Test
+    public void getResourceIdNextReturningNonNullWithAttributesToMerge() throws Exception {
+        Method method = ResourceDiscoverMock.class.getMethod("m1");
+        ResourceDiscoverMock resourceDiscoverMock = new ResourceDiscoverMock();
+        resourceDiscoverMock.structure = Structure.create("structures");
+        
+        
+        // first call
+        CachedResourceDiscover cachedResourceDiscover = new CachedResourceDiscover(resourceDiscoverMock);
+        cachedResourceDiscover.addAttributeToMerge(method, new Attribute(AttributeName.VERSION,"1.0.0"));
+
+        Structure cachedStructure =  cachedResourceDiscover.getStructure(method);
+      
+        assertEquals(resourceDiscoverMock.structure.getName(), cachedStructure.getName());
+        assertTrue(resourceDiscoverMock.structure.getAttributes().isEmpty());
+        assertEquals(cachedStructure.getAttributes().size(),1);
+        assertEquals(AttributeName.VERSION, cachedStructure.getAttributes().get(0).getName());
+        assertEquals("1.0.0", cachedStructure.getAttributes().get(0).getValue());
+        
+        assertEquals(1,resourceDiscoverMock.timesGetResoureId);
+        assertEquals(0,resourceDiscoverMock.timesGetOperation);
+        assertEquals(method,resourceDiscoverMock.lastMethodParameter);
+        
+        // second call
+        assertEquals(resourceDiscoverMock.structure.getName(), cachedStructure.getName());
+        assertTrue(resourceDiscoverMock.structure.getAttributes().isEmpty());
+        assertEquals(cachedStructure.getAttributes().size(),1);
+        assertEquals(AttributeName.VERSION, cachedStructure.getAttributes().get(0).getName());
+        assertEquals("1.0.0", cachedStructure.getAttributes().get(0).getValue());
+
+        assertEquals(1,resourceDiscoverMock.timesGetResoureId);
+        assertEquals(0,resourceDiscoverMock.timesGetOperation);
+        assertEquals(method,resourceDiscoverMock.lastMethodParameter);
+        
+    }
+
  
     @Test
     public void getResourceOperationNextReturningNull() throws Exception {
